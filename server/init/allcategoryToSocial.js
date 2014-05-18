8a47c1db2022bf39b7bb41b56277b950b8aacf4c
mue <mue@gmail.com>
2014-05-18 16:25:55 -0400

    change category

---
+++ server/init/allcategoryToSocial.js
@@ -0,0 +1,18 @@
+var config = require('config')
+    , HttpError = require('error').HttpError
+    , _ = require('underscore'),
+    async = require('async'),
+    IssueModel = require('models/issue/issue');
+
+console.log('start');
+
+require("mongooseDb");
+
+IssueModel.update({}, {category: "5378fa92124279bb200cb325"}, {multi: true}, function(err){
+    console.log(err);
+    if(err){
+        console.log('false');
+        return false;
+    }
+    console.log('done')
+});
